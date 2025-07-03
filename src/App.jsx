fbf4d211d667c344afdc100d6abd3f664932f1e4
Ansh_sharma <keshu1234harshu@gmail.com>
2025-07-04 03:44:19 +0530

    basic navbar half done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-black w-[100%] text-pretty mx-auto lg:pl-[200px] lg:pr-[200px] relative '>
+      <div className='container mx-auto px-4 py-8 relative'>
+        <Navbar/>
+      </div>
+    </div>
+  )
+}
+
+export default App
