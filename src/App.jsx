b905f80fab96f97b8befa3b23de3c20b4251a2a9
Amaan Sayyed <amaanafsarsayyed@gmail.com>
2025-07-06 12:22:33 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import About from './components/About'
+import Skills from './components/Skills'
 
 const App = () => {
   return (
     <div className='min-h-screen bg-base-300 w-[100%] text-pretty mx-auto lg:pl-[200px] lg:pr-[200px] relative '>
       <div className='container mx-auto px-4 py-8 relative'>
-        <Navbar/>
+        <nav><Navbar/></nav>
+        <section><About/></section>
+        {/* <section>Work Experience</section> */}
+        <section><Skills/></section>
       </div>
     </div>
   )
---
+++ src/components/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <div className='flex flex-col gap-2 mb-8 backdrop:blur-sm rounded-lg border-2 border-base-100 p-2 bg-base-200 shadow-sm'>
+      Skills
+    </div>
+  )
+}
+
+export default Skills
