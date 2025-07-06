c19013e7303984e51bc6c6ac4e38758f1bd8f44d
Ansh_sharma <keshu1234harshu@gmail.com>
2025-07-06 11:05:34 +0530

    svg size pushh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
                 {/* sun icon */}
                 <svg
-                    className="swap-off h-7 w-7 fill-current"
+                    className="swap-off h-6 w-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24">
                     <path
@@ -24,7 +24,7 @@ const Navbar = () => {
 
                 {/* moon icon */}
                 <svg
-                    className="swap-on h-7 w-7 fill-current"
+                    className="swap-on h-6 w-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24">
                     <path
