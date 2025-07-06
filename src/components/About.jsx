0a50dd8f4c2ce264f226bcfe847e35de949bf043
Ansh_sharma <keshu1234harshu@gmail.com>
2025-07-06 18:39:10 +0530

    minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div className="flex flex-col gap-2 mb-5 backdrop:blur-sm rounded-lg border-2 border-base-100 p-2 bg-base-200 shadow-sm">
+    <div className="flex flex-col gap-2 mb-4 backdrop:blur-sm rounded-lg border-2 border-base-100 p-2 bg-base-200 shadow-sm">
       <h2 className='text-base ml-3 font-bold'>About Me</h2>
-      <p className='ml-3 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nobis distinctio dolores ducimus esse magni vitae pariatur quam adipisci quisquam animi temporibus, eaque similique repellendus laborum natus fuga culpa dignissimos.</p>
+      <p className='ml-3 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nobis distinctio dolores ducimus vitae pariatur quam adipisci quisquam animi temporibus,laborum natus fuga culpa dignissimos.</p>
     </div>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,14 +5,14 @@ function Skills() {
     <div className='flex flex-col gap-2 mb-8 backdrop:blur-sm rounded-lg border-2 border-base-100 p-2 bg-base-200 shadow-sm'>
       <h2 className='text-base ml-3 font-bold'>Skills</h2>
       <div className='flex flex-wrap ml-1.5'>
-        <div className='badge badge-outline badge-info m-1 transition-all duration-300 ease-in-out hover:badge-soft'>JavaScript</div>
-        <div className='badge badge-outline badge-info m-1 transition-all duration-300 ease-in-out hover:badge-soft'>React</div>
-        <div className='badge badge-outline badge-info m-1 transition-all duration-300 ease-in-out hover:badge-soft'>Node.js</div>
-        <div className='badge badge-outline badge-info m-1 transition-all duration-300 ease-in-out hover:badge-soft'>Express</div>
-        <div className='badge badge-outline badge-info m-1 transition-all duration-300 ease-in-out hover:badge-soft'>MongoDB</div>
-        <div className='badge badge-outline badge-info m-1 transition-all duration-300 ease-in-out hover:badge-soft'>CSS</div>
-        <div className='badge badge-outline badge-info m-1 transition-all duration-300 ease-in-out hover:badge-soft'>HTML</div>
-        <div className='badge badge-outline badge-info m-1 transition-all duration-300 ease-in-out hover:badge-soft'>Git</div>
+        <div className='badge badge-info m-1 transition-all duration-300 ease-in-out hover:badge-outline hover:scale-102 hover:-translate-y-[2px] font-semibold '>JavaScript</div>
+        <div className='badge badge-info m-1 transition-all duration-300 ease-in-out hover:badge-outline hover:scale-102 hover:-translate-y-[2px] font-semibold '>React</div>
+        <div className='badge badge-info m-1 transition-all duration-300 ease-in-out hover:badge-outline hover:scale-102 hover:-translate-y-[2px] font-semibold '>Node.js</div>
+        <div className='badge badge-info m-1 transition-all duration-300 ease-in-out hover:badge-outline hover:scale-102 hover:-translate-y-[2px] font-semibold '>Express</div>
+        <div className='badge badge-info m-1 transition-all duration-300 ease-in-out hover:badge-outline hover:scale-102 hover:-translate-y-[2px] font-semibold '>MongoDB</div>
+        <div className='badge badge-info m-1 transition-all duration-300 ease-in-out hover:badge-outline hover:scale-102 hover:-translate-y-[2px] font-semibold '>CSS</div>
+        <div className='badge badge-info m-1 transition-all duration-300 ease-in-out hover:badge-outline hover:scale-102 hover:-translate-y-[2px] font-semibold '>HTML</div>
+        <div className='badge badge-info m-1 transition-all duration-300 ease-in-out hover:badge-outline hover:scale-102 hover:-translate-y-[2px] font-semibold '>Git</div>
       </div>
     </div>
   )
