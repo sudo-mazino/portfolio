c7333c4dc9f63cadd3db82f18a8640cdf9a94796
Ansh_sharma <keshu1234harshu@gmail.com>
2025-07-06 11:36:37 +0530

    links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Linkedin } from 'lucide-react'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center mb-8 backdrop:blur-sm rounded-lg navbar border-2 border-base-100 p-4 bg-base-200 shadow-sm">
+    <div className="flex justify-between items-center mb-8 backdrop:blur-sm rounded-lg navbar border-2 border-base-100 p-2 bg-base-200 shadow-sm">
         <div className='flex flex-col gap-1 ml-3'>
             <h1 className='text-2xl font-bold'>Name</h1>
             <p className='text-[13px] '>Full stack engineer</p>
         </div>
-        
+        <div>
+            <ul className="menu menu-horizontal px-1">
+                <li><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg></a></li>
+                <li><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg></a></li>
+                <li><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg></a></li>
+                <li><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone-icon lucide-phone"><path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"/></svg></a></li>
+            </ul>
+        </div>     
         <div className='flex items-center gap-2'>
             <label className="swap swap-rotate">
                 {/* this hidden checkbox controls the state */}
